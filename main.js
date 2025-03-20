f87bea7a5ffa34fb516411973031b2c0922f9bef
Ironboy <ironboy@users.noreply.github.com>
2025-03-20 14:36:30 +0100

    Connection to db working :)

--- main.js
+++ main.js
@@ -4,30 +4,10 @@ import dbQuery from "./libs/dbQuery.js";
 import tableFromData from './libs/tableFromData.js'
 
 addMdToPage(`
-  ## petOwners
-  En tabell över husdjursägare.
+  # The first 10 students
+  **Note:** Only the first four columns so far...
 `);
 
-let petOwners = await dbQuery('SELECT * FROM petOwners');
-tableFromData({ data: petOwners });
+let someStudents = await dbQuery('SELECT * FROM results LIMIT 10');
 
-
-addMdToPage(`
-  ## pets
-  En tabell över husdjur.
-`);
-
-let pets = await dbQuery('SELECT * FROM pets');
-tableFromData({ data: pets });
-
-
-addMdToPage(`
-  ## petOwnersAndPets
-  En vy över vilka husdjursägare som äger vilka husdjur.
-  * Här kan man se vilka husdjursägare som äger vilka husdjur.
-  * Men: Husdjursägare utan husdjur visas inte.
-  * Och: Husdjur ägare utan ägare visas inte heller.
-`);
-
-let petOwnersAndPets = await dbQuery('SELECT * FROM petOwnersAndPets');
-tableFromData({ data: petOwnersAndPets });
+tableFromData({ data: someStudents });
